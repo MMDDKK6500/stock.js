03302150a3413e0b00081aca123733f269b53106
MMDDKK6500 <woobiewhy@gmail.com>
2023-09-22 10:34:06 -0300

    prepare for publish

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { Company } from './Company';
 import { Share } from "./Share"
 import { Buyer } from "./Buyer";
+import { StockStatus } from "./StockStatus";
 
-let apple = new Company("Apple")
-let me = new Buyer("Me", "1234")
-
-console.log(apple.buyStocks(10, me, "1234"))
-console.log(me.shares)
-console.log(apple.sharesAvailable)
-console.log(apple.sellStocks(10, me, "1234"))
-console.log(me.shares)
-console.log(apple.sharesAvailable)
+export * from './Company';
+export * from './Share';
+export * from './Buyer';
+export * from './StockStatus';
